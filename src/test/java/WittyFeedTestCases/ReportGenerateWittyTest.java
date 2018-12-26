a25c846f7cbee0a255ea781ebeb0c108210b252f
Shubham Sharma <43319849+flyboy0808@users.noreply.github.com>
2018-12-26 13:24:36 +0530

    wfhook

--- src/test/java/WittyFeedTestCases/ReportGenerateWittyTest.java
+++ src/test/java/WittyFeedTestCases/ReportGenerateWittyTest.java
@@ -35,7 +35,7 @@ public class ReportGenerateWittyTest {
 	  		htmlReporter = new ExtentHtmlReporter("WittyfeedTestResult-Automation.html");
 	  
 	  		// For windows
-//		    htmlReporter = new ExtentHtmlReporter("WebsiteTestResult-Automation.html");
+//		    htmlReporter = new ExtentHtmlReporter("WebsiteTestResult-Automation.html"); test it
 
 		    extent = new ExtentReports();
 		    extent.attachReporter(htmlReporter);
